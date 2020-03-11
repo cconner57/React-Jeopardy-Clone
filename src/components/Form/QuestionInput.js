fd87df529cd43884492db09fdee011ac7fdb1797
Chris Conner <chris.conner57@gmail.com>
2020-03-10 21:54:14 -0700

    Fix form styling

--- src/components/Form/QuestionInput.js
+++ src/components/Form/QuestionInput.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function FormInput2({category, prevStep, nextStep, onChange}) {
+function FormInput2({category, nextStep, onChange}) {
     const next = e => {
         e.preventDefault();
         nextStep();
@@ -15,20 +15,16 @@ function FormInput2({category, prevStep, nextStep, onChange}) {
                         <div className="Input-Group">
                             <input type="text" placeholder="$100 Question" onChange={onChange} />
                             <input type="text" placeholder="$100 Answer" onChange={onChange} />
-                        </div>
-                        <div className="Input-Group">
+                            <hr/>
                             <input type="text" placeholder="$200 Question" onChange={onChange} />
                             <input type="text" placeholder="$200 Answer" onChange={onChange} />
-                        </div>
-                        <div className="Input-Group">
+                            <hr/>
                             <input type="text" placeholder="$300 Question" onChange={onChange} />
                             <input type="text" placeholder="$300 Answer" onChange={onChange} />
-                        </div>
-                        <div className="Input-Group">
+                            <hr/>
                             <input type="text" placeholder="$400 Question" onChange={onChange} />
                             <input type="text" placeholder="$400 Answer" onChange={onChange} />
-                        </div>
-                        <div className="Input-Group">
+                            <hr/>
                             <input type="text" placeholder="$500 Question" onChange={onChange} />
                             <input type="text" placeholder="$500 Answer" onChange={onChange} />
                         </div>
